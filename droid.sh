ff3dc30dfea37234484cc91b7a99f0368a485762
gabriele.lana <gabriele.lana@cleancode.it>
2013-10-26 11:37:43 +0200

    add pomicons.ttf as symbols font

--- droid.sh
+++ droid.sh
@@ -3,7 +3,8 @@
 cp ./fonts/fontawesome-webfont.ttf S0.ttf
 ./patch S0.ttf --symbols=./fonts/octicons-regular.ttf --rename-as=S1 --starting-at='0xf200' --ratio=1.1 --shift-y=-100
 ./patch S1.ttf --symbols=./fonts/trellicons-regular.ttf --rename-as=S2 --starting-at='0xf400' --ratio=0.9 --shift-y=150
-./patch ./fonts/Droid+Sans+Mono+for+Powerline.ttf --symbols=S2.ttf --rename-as='Droid Sans Mono Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=200 --shift-y=-300
+./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --rename-as=S3 --starting-at='0xf600' --ratio=0.85
+./patch ./fonts/Droid+Sans+Mono+for+Powerline.ttf --symbols=S3.ttf --rename-as='Droid Sans Mono Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=200 --shift-y=-250
 cp Droid+Sans+Mono+Awesome.ttf ~/.fonts
 mv Droid+Sans+Mono+Awesome.ttf ./patched
 sudo fc-cache -rv
--- inconsolata.sh
+++ inconsolata.sh
@@ -3,7 +3,8 @@
 cp ./fonts/fontawesome-webfont.ttf S0.ttf
 ./patch S0.ttf --symbols=./fonts/octicons-regular.ttf --rename-as=S1 --starting-at='0xf200' --shift-y=-50
 ./patch S1.ttf --symbols=./fonts/trellicons-regular.ttf --rename-as=S2 --starting-at='0xf400' 
-./patch ./fonts/Inconsolata+for+Powerline.ttf --symbols=S2.ttf --rename-as='Inconsolata Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=100
+./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --rename-as=S3 --starting-at='0xf600' --ratio=0.85
+./patch ./fonts/Inconsolata+for+Powerline.ttf --symbols=S3.ttf --rename-as='Inconsolata Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=100 --shift-y=20
 cp Inconsolata+Awesome.ttf ~/.fonts
 mv Inconsolata+Awesome.ttf ./patched
 sudo fc-cache -rv
