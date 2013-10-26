8552f408e5c9a9c32f4b1f444145f49d14f5a7ed
gabriele.lana <gabriele.lana@cleancode.it>
2013-10-26 16:08:05 +0200

    Inconsolata patched and mapped with updated awesome/octicons fonts

--- inconsolata.sh
+++ inconsolata.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cp ./fonts/fontawesome-webfont.ttf S0.ttf
-./patch S0.ttf --symbols=./fonts/octicons-regular.ttf --rename-as=S1 --starting-at='0xf200' --shift-y=-50
-./patch S1.ttf --symbols=./fonts/trellicons-regular.ttf --rename-as=S2 --starting-at='0xf400' 
-./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --rename-as=S3 --starting-at='0xf600' --ratio=0.85
-./patch ./fonts/Inconsolata+for+Powerline.ttf --symbols=S3.ttf --rename-as='Inconsolata Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=100 --shift-y=20
-cp Inconsolata+Awesome.ttf ~/.fonts
-mv Inconsolata+Awesome.ttf ./patched
+cp ./fonts/Inconsolata+for+Powerline.ttf S0.ttf
+./patch S0.ttf --symbols=./fonts/fontawesome-webfont.ttf --to-namespace=AWESOME --rename-as=S1 --starting-at='0xe100' --ratio=0.85 --shift-x=100 --shift-y=20
+./patch S1.ttf --symbols=./fonts/octicons-regular.ttf --to-namespace=OCTICONS --rename-as=S2 --starting-at='0xe800' --ratio=0.85 --shift-y=-50
+./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --to-namespace=POMICONS --rename-as='Inconsolata Awesome' --starting-at='0xf000' --ratio=0.85
+cat S1.sh S2.sh >> Inconsolata+Awesome.sh
+cp Inconsolata+Awesome.* ~/.fonts
+mv Inconsolata+Awesome.* ./patched
 sudo fc-cache -rv
-rm -rf S?.ttf
+rm -rf S?.*
