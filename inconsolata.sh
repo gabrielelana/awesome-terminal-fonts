1d790f57d3eb3a419fd871a175cef2ceaf23728e
gabriele.lana <gabriele.lana@cleancode.it>
2013-10-26 17:00:03 +0200

    copy also name -> codepoint map to local fonts directory

--- inconsolata.sh
+++ inconsolata.sh
@@ -5,7 +5,7 @@ cp ./fonts/Inconsolata+for+Powerline.ttf S0.ttf
 ./patch S1.ttf --symbols=./fonts/octicons-regular.ttf --to-namespace=OCTICONS --rename-as=S2 --starting-at='0xe800' --ratio=0.85 --shift-y=-50
 ./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --to-namespace=POMICONS --rename-as='Inconsolata Awesome' --starting-at='0xf000' --ratio=0.85
 cat S1.sh S2.sh >> Inconsolata+Awesome.sh
-cp Inconsolata+Awesome.ttf ~/.fonts
+cp Inconsolata+Awesome.* ~/.fonts
 mv Inconsolata+Awesome.* ./patched
 sudo fc-cache -rv
 rm -rf S?.*
