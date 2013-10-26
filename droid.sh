77290e302f021fc245a0661039f77162f216bade
gabriele.lana <gabriele.lana@cleancode.it>
2013-10-26 16:58:17 +0200

    Droid Sans Mono patched and mapped with updated awesome/octicons fonts

--- droid.sh
+++ droid.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-cp ./fonts/fontawesome-webfont.ttf S0.ttf
-./patch S0.ttf --symbols=./fonts/octicons-regular.ttf --rename-as=S1 --starting-at='0xf200' --ratio=1.1 --shift-y=-100
-./patch S1.ttf --symbols=./fonts/trellicons-regular.ttf --rename-as=S2 --starting-at='0xf400' --ratio=0.9 --shift-y=150
-./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --rename-as=S3 --starting-at='0xf600' --ratio=0.85
-./patch ./fonts/Droid+Sans+Mono+for+Powerline.ttf --symbols=S3.ttf --rename-as='Droid Sans Mono Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=200 --shift-y=-250
+cp ./fonts/Droid+Sans+Mono+for+Powerline.ttf S0.ttf
+./patch S0.ttf --symbols=./fonts/fontawesome-webfont.ttf --to-namespace=AWESOME --rename-as=S1 --starting-at='0xe100' --ratio=0.9 --shift-x=200 --shift-y=-300
+./patch S1.ttf --symbols=./fonts/octicons-regular.ttf --to-namespace=OCTICONS --rename-as=S2 --starting-at='0xe800' --ratio=0.85 --shift-x=-100 --shift-y=-100
+./patch S2.ttf --symbols=./fonts/pomicons-regular.ttf --to-namespace=POMICONS --rename-as='Droid Sans Mono Awesome' --starting-at='0xf000' --ratio=0.85 --shift-y=-400 --shift-x=-600
+cat S1.sh S2.sh >> Droid+Sans+Mono+Awesome.sh
 cp Droid+Sans+Mono+Awesome.ttf ~/.fonts
-mv Droid+Sans+Mono+Awesome.ttf ./patched
+mv Droid+Sans+Mono+Awesome.* ./patched
 sudo fc-cache -rv
-rm -rf S?.ttf
+rm -rf S?.*
+
+# cp ./fonts/fontawesome-webfont.ttf S0.ttf
+# ./patch S0.ttf --symbols=./fonts/octicons-regular.ttf --rename-as=S1 --starting-at='0xf200' --ratio=1.1 --shift-y=-100
+# ./patch S1.ttf --symbols=./fonts/trellicons-regular.ttf --rename-as=S2 --starting-at='0xf400' --ratio=0.9 --shift-y=150
+# ./patch ./fonts/Droid+Sans+Mono+for+Powerline.ttf --symbols=S3.ttf --rename-as='Droid Sans Mono Awesome' --starting-at='0xe100' --ratio=0.85 --shift-x=200 --shift-y=-250
+# cp Droid+Sans+Mono+Awesome.ttf ~/.fonts
+# mv Droid+Sans+Mono+Awesome.ttf ./patched
+# sudo fc-cache -rv
+# rm -rf S?.ttf
