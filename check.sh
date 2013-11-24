f6f2a8b78deb55166a34b6fbee121b3fbdb6ef8d
gabriele.lana <gabriele.lana@cleancode.it>
2013-11-24 19:27:21 +0100

    wider range for check utility

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-for shift in {0..700}; do echo -ne "`printf ' \\\\u%x' $((0xe100 + $shift))`"; done
+for shift in {0..6000}; do echo -ne "`printf ' \\\\u%x' $((0xe000 + $shift))`"; done
