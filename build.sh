1dc289f5be5198e075cab5ad51251c333a1b6d25
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-04 17:47:40 +0100

    script/fu-map creates maps files between glyph name and its codepoint

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./scripts/fu-map ./fonts/pomicons-regular.ttf --namespace 'POMICONS' 2>/dev/null > ./maps/pomicons-regular.sh
+./scripts/fu-map ./fonts/octicons-regular.ttf --namespace 'OCTICONS' 2>/dev/null > ./maps/octicons-regular.sh
+./scripts/fu-map ./fonts/fontawesome-webfont.ttf --namespace 'AWESOME' 2>/dev/null > ./maps/fontawesome-webfont.sh
