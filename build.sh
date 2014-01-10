115415cdb39c50f00006ff7ca16c928b8cefee0f
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-10 15:57:23 +0100

    new build script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-./scripts/fu-map ./fonts/pomicons-regular.ttf --namespace 'POMICONS' 2>/dev/null > ./maps/pomicons-regular.sh
-./scripts/fu-map ./fonts/octicons-regular.ttf --namespace 'OCTICONS' 2>/dev/null > ./maps/octicons-regular.sh
-./scripts/fu-map ./fonts/fontawesome-webfont.ttf --namespace 'AWESOME' 2>/dev/null > ./maps/fontawesome-webfont.sh
+echo 'build all symbols fonts...'
+cp -rf ./fonts/* ./build/
+
+echo 'export maps for all fonts...'
+./scripts/fu-map ./build/pomicons-regular.ttf --namespace 'POMICONS' 2>/dev/null > ./build/pomicons-regular.sh
+./scripts/fu-map ./build/octicons-regular.ttf --namespace 'OCTICONS' 2>/dev/null > ./build/octicons-regular.sh
+./scripts/fu-map ./build/fontawesome-webfont.ttf --namespace 'AWESOME' 2>/dev/null > ./build/fontawesome-webfont.sh
+
+echo 'you can find fonts and maps in local ./build directory :-)'
+echo 'done!'
