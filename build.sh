f9e8e2a0d928b624267c6f02150b33ecc3bb4600
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-12 22:21:47 +0100

    updated build script with octicons font relocation to avoid clash with the awesome font

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
+rm -f ./build/*
+mkdir -p ./.work
+
 echo 'build all symbols fonts...'
-cp -rf ./fonts/* ./build/
+cp ./fonts/pomicons-regular.ttf ./build
+cp ./fonts/fontawesome-webfont.ttf ./build
+
+./scripts/fu-relocate ./fonts/octicons-regular.ttf --save-as='.work/octicons-regular-relocated.ttf' --to='0xf200' 2> /dev/null
+cp ./.work/octicons-regular-relocated.ttf ./build/octicons-regular.ttf
+
 
 echo 'export maps for all fonts...'
-./scripts/fu-map ./build/pomicons-regular.ttf --namespace 'POMICONS' 2>/dev/null > ./build/pomicons-regular.sh
-./scripts/fu-map ./build/octicons-regular.ttf --namespace 'OCTICONS' 2>/dev/null > ./build/octicons-regular.sh
-./scripts/fu-map ./build/fontawesome-webfont.ttf --namespace 'AWESOME' 2>/dev/null > ./build/fontawesome-webfont.sh
+./scripts/fu-map ./build/pomicons-regular.ttf --namespace 'POMICONS' 2> /dev/null > ./build/pomicons-regular.sh
+./scripts/fu-map ./build/octicons-regular.ttf --namespace 'OCTICONS' 2> /dev/null > ./build/octicons-regular.sh
+./scripts/fu-map ./build/fontawesome-webfont.ttf --namespace 'AWESOME' 2> /dev/null > ./build/fontawesome-webfont.sh
 
 echo 'you can find fonts and maps in local ./build directory :-)'
 echo 'done!'
