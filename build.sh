e7c89834fa58a593c7a4db8fda7f61cf82a84fa1
gabriele.lana <gabriele.lana@cleancode.it>
2016-07-23 11:04:14 +0200

    Update to FontAwesome 4.6.3

--- build.sh
+++ build.sh
@@ -10,9 +10,8 @@ cp ./fonts/pomicons-regular.ttf ./build
 cp ./fonts/fontawesome-regular.ttf ./build
 cp ./fonts/devicons-regular.ttf ./build
 
-./scripts/fu-relocate ./fonts/octicons-regular.ttf --save-as='.work/octicons-regular-relocated.ttf' --to='0xf200' 2> /dev/null
+./scripts/fu-relocate ./fonts/octicons-regular.ttf --save-as='.work/octicons-regular-relocated.ttf' --to='0xf300' 2> /dev/null
 cp ./.work/octicons-regular-relocated.ttf ./build/octicons-regular.ttf
-
 
 echo 'export maps for all fonts...'
 ./scripts/fu-map ./build/pomicons-regular.ttf --namespace 'POMICONS' 2> /dev/null > ./build/pomicons-regular.sh
